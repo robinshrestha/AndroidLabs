f4a2e775d97e4cd422a0951ed5c1b1dcd5191dec
robinshrstha <robinshrestha.oslt@gmail.com>
2018-04-13 16:26:04 -0400

    workinglab8

--- app/src/main/java/com/example/robin/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/robin/androidlabs/TestToolbar.java
@@ -31,7 +31,7 @@ public class TestToolbar extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your word", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
